07097b0f0098b600db111e5e4dbdd9bd358841ff
Pedro Henrique <pedro2266809ph@gmail.com>
2024-01-28 11:44:34 -0300

    feat: addicionando config para erro de cors

--- src/main/java/com/Itaubackendchallenge/controller/PessoaController.java
+++ src/main/java/com/Itaubackendchallenge/controller/PessoaController.java
@@ -3,13 +3,16 @@ package com.Itaubackendchallenge.controller;
 import com.Itaubackendchallenge.dto.PessoaDTO;
 import com.Itaubackendchallenge.entities.Pessoa;
 import com.Itaubackendchallenge.services.PessoaService;
+import org.hibernate.query.Page;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Limit;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/pessoa")
@@ -24,5 +27,11 @@ public class PessoaController {
         Pessoa pessoa = pessoaService.insert(pessoaDTO);
         return ResponseEntity.ok().body(pessoa);
     }
+
+    @GetMapping
+    public ResponseEntity<List<Pessoa>> buscaTodos() {
+        return ResponseEntity.ok().body(pessoaService.buscaTodos());
+    }
+
 }
 
--- src/main/java/com/Itaubackendchallenge/services/PessoaService.java
+++ src/main/java/com/Itaubackendchallenge/services/PessoaService.java
@@ -5,7 +5,12 @@ import com.Itaubackendchallenge.entities.Pessoa;
 import com.Itaubackendchallenge.exceptions.ApExceptionHandler;
 import com.Itaubackendchallenge.repositories.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class PessoaService {
@@ -13,11 +18,12 @@ public class PessoaService {
     @Autowired
     private PessoaRepository repository;
 
-
+    @Transactional
     public Pessoa insert(PessoaDTO pessoaDTO)    {
+
         Pessoa pessoa = new Pessoa();
 
-        if(pessoa == null || pessoa.getPrimeiroNome() == null) {
+        if(pessoaDTO == null || pessoaDTO.primeiroNome() == null) {
             throw new NullPointerException();
         }
 
@@ -27,5 +33,14 @@ public class PessoaService {
 
         return repository.save(pessoa);
     }
+
+
+    @Transactional(readOnly = true)
+    public List<Pessoa> buscaTodos() {
+        return repository.findAll();
+    }
+
+
+
 
 }
